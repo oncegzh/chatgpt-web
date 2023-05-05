72421fb57792d34226ea76b9c424b007e6c922a6
oncegzh <2679780735@qq.com>
2023-05-05 11:32:01 +0800

    chore: 去除文本省略号

--- src/components/common/UserAvatar/index.vue
+++ src/components/common/UserAvatar/index.vue
@@ -29,7 +29,7 @@ const userInfo = computed(() => userStore.userInfo)
       <h2 class="overflow-hidden font-bold text-md text-ellipsis whitespace-nowrap">
         {{ userInfo.name ?? 'BIFROST' }}
       </h2>
-      <p class="overflow-hidden text-xs text-gray-500 text-ellipsis whitespace-nowrap">
+      <p class="overflow-hidden text-xs text-gray-500 whitespace-nowrap">
         <span
           v-if="isString(userInfo.description) && userInfo.description !== ''"
           v-html="userInfo.description"
